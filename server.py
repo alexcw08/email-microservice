a5c39f1452416830bd5a9bd6678372e9c9705d24
Alex C <calex781@gmail.com>
2023-08-01 10:16:18 -0400

    Remove empty line print statements. Move html f string into its own function.

--- server.py
+++ server.py
@@ -8,38 +8,7 @@ PORT = 65432
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 
-
-while True:
-    server.listen(1)
-    print(f'Server listening on port: {PORT}')
-    print('')
-    # receive connection
-    commSocket, addr = server.accept()
-    print(f"Connected by {addr}")
-    print('')
-
-    # receive ledger data
-    dataMsg = commSocket.recv(1024).decode()
-
-    print('Server received:', dataMsg)
-    print('')
-    # convert string to dictionary
-    dataMsg = json.loads(dataMsg)
-    # extract the necessary info from data
-    title = dataMsg['ledger'][0]['title']
-    date = dataMsg['ledger'][4]['date']
-    people = []
-    for person in dataMsg['ledger'][2]['persons']:
-        people.append(person['name'])
-    peopleString = ', '.join(people)
-    summary = dataMsg['ledger'][1]['summary']
-    items = dataMsg['ledger'][3]['items']
-    htmlTable = []
-    for item in items:
-        htmlTable.append(f"<tr><td>{item['item']}</td><td>{item['amount']}</td><td>{item['date']}</td><td>{item['paid_by']}</td></tr>")
-    htmlTable = ''.join(htmlTable)
-
-    # f string containing the entire html
+def generateHTML(title, date, people, summary, table):
     html = f'''<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -50,7 +19,7 @@ while True:
     </head>
     <body>
         <h1>Subject: {title}</h1>
-        <p>You have been sent a record of shared expenses between {peopleString}. The following is a snapshop of that ledger from {date}</p>
+        <p>You have been sent a record of shared expenses between {people}. The following is a snapshop of that ledger from {date}</p>
         <table style="width:80%">
     <tr>
         <th>Item</th>
@@ -58,12 +27,41 @@ while True:
         <th>Date</th>
         <th>Paid By</th>
     </tr>
-    {htmlTable}
+    {table}
     </table>
     <p>Note that items with an " * " means they have been edited. Items with "del" have been removed from the summary.</p>
         <h2>Ledger summary: {summary}</h2>
     </body>
     </html>'''
+
+    return html
+
+while True:
+    server.listen(1)
+    print(f'Server listening on port: {PORT} \n')
+    commSocket, addr = server.accept()
+    print(f'Connected by {addr} \n')
+
+    ledgerData = commSocket.recv(1024).decode()
+    print(f'Server received: {ledgerData}\n')
+
+    # convert string to dictionary
+    ledgerData = json.loads(ledgerData)
+    # extract the necessary info from data
+    title = ledgerData['ledger'][0]['title']
+    date = ledgerData['ledger'][4]['date']
+    people = []
+    for person in ledgerData['ledger'][2]['persons']:
+        people.append(person['name'])
+    people = ', '.join(people)
+    summary = ledgerData['ledger'][1]['summary']
+    items = ledgerData['ledger'][3]['items']
+    htmlTable = []
+    for item in items:
+        htmlTable.append(f"<tr><td>{item['item']}</td><td>{item['amount']}</td><td>{item['date']}</td><td>{item['paid_by']}</td></tr>")
+    htmlTable = ''.join(htmlTable)
+
+    html = generateHTML(title, date, people, summary, htmlTable)
     commSocket.send(html.encode())
     print('sending html')
     commSocket.close()
