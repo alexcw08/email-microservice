8ebd7ceed0232eb0d47a7eb648d99a4786582651
Alex C <calex781@gmail.com>
2023-08-09 10:46:37 -0400

    Implement server receiving and verifying the length of data to be received. Move functions above socket set up.

--- client.py
+++ client.py
@@ -11,108 +11,163 @@ print(f"Connected to server:{HOST}:{PORT}... \n")
 
 # json to test with
 ledgerJSON = {
-    "ledger": [
-        {
-            "title": "Just cussin' around."
-        },
-        {
-            "summary": ""
-        },
-        {
-            "persons": [
-                {
-                    "name": "Randy Marsh",
-                    "email": "randy@sp.com"
-                },
-                {
-                    "name": "Heath Ledger",
-                    "email": "hl@dk.com"
-                },
-                {
-                    "name": "Stanley Kubrick",
-                    "email": "sfk@movie.god.com"
-                }
-            ]
-        },
-        {
-            "items": [
-                {
-                    "item": "Go Karts",
-                    "amount": "350.00",
-                    "date": "2008-04-01",
-                    "paid_by": "Randy Marsh"
-                },
-                {
-                    "item": "Brewskies",
-                    "amount": "91.55",
-                    "date": "2008-04-01",
-                    "paid_by": "Heath Ledger"
-                },
-                {
-                    "item": "Hotdog Competition",
-                    "amount": "33.00",
-                    "date": "2008-10-20",
-                    "paid_by": "Stanley Kubrick"
-                },
-                {
-                    "item": "(del)L",
-                    "amount": "900,000.00",
-                    "date": "2009-06-02",
-                    "paid_by": "Stanley Kubrick"
-                },
-                {
-                    "item": "River Cruise",
-                    "amount": "1,900.00****",
-                    "date": "2011-10-19",
-                    "paid_by": "Stanley Kubrick"
-                },
-                {
-                    "item": "Video Games",
-                    "amount": "60.00",
-                    "date": "2012-08-30",
-                    "paid_by": "Heath Ledger"
-                },
-                {
-                    "item": "Brews",
-                    "amount": "32.00",
-                    "date": "2012-05-01",
-                    "paid_by": "Randy Marsh"
-                },
-                {
-                    "item": "Golf",
-                    "amount": "800.00",
-                    "date": "2013-07-26",
-                    "paid_by": "Heath Ledger"
-                }
-            ]
-        },
-        {
-            "date": "2023-07-25"
-        }
-    ]
+    "ledger": {
+        "title": "Second Ledger",
+        "date": "2023-08-08",
+        "people": [
+            {
+                "name": "Heath Ledger",
+                "email": "hl@fakemail.com"
+            },
+            {
+                "name": "Johanna W",
+                "email": "jw@fakemail.com"
+            },
+            {
+                "name": "James S.A. Corey",
+                "email": "jsac@fakemail.com"
+            }
+        ],
+        "summary": "Expense total: $3294.16\tTotal per person: $1098.05\nHeath Ledger owes $1087.8. Johanna W owes $848.05. James S.A. Corey is owed $1935.86. ",
+        "transactions": [
+            {
+                "item": "First transaction",
+                "amount": "10.25",
+                "date": "2023-04-01*",
+                "paid_by": "Heath Ledger"
+            },
+            {
+                "item": "Little Purchase",
+                "amount": "3000.21",
+                "date": "2023-05-20",
+                "paid_by": "James S.A. Corey"
+            },
+            {
+                "item": "Mystery Items",
+                "amount": "250.00",
+                "date": "2023-06-08*",
+                "paid_by": "Johanna W"
+            },
+            {
+                "item": "Fourth Transaction",
+                "amount": "33.70",
+                "date": "2023-07-23",
+                "paid_by": "James S.A. Corey"
+            }
+        ]
+    }
 }
+# ledgerJSON = {
+#     "ledger": [
+#         {
+#             "title": "Just cussin' around."
+#         },
+#         {
+#             "summary": ""
+#         },
+#         {
+#             "persons": [
+#                 {
+#                     "name": "Randy Marsh",
+#                     "email": "randy@sp.com"
+#                 },
+#                 {
+#                     "name": "Heath Ledger",
+#                     "email": "hl@dk.com"
+#                 },
+#                 {
+#                     "name": "Stanley Kubrick",
+#                     "email": "sfk@movie.god.com"
+#                 }
+#             ]
+#         },
+#         {
+#             "items": [
+#                 {
+#                     "item": "Go Karts",
+#                     "amount": "350.00",
+#                     "date": "2008-04-01",
+#                     "paid_by": "Randy Marsh"
+#                 },
+#                 {
+#                     "item": "Brewskies",
+#                     "amount": "91.55",
+#                     "date": "2008-04-01",
+#                     "paid_by": "Heath Ledger"
+#                 },
+#                 {
+#                     "item": "Hotdog Competition",
+#                     "amount": "33.00",
+#                     "date": "2008-10-20",
+#                     "paid_by": "Stanley Kubrick"
+#                 },
+#                 {
+#                     "item": "(del)L",
+#                     "amount": "900,000.00",
+#                     "date": "2009-06-02",
+#                     "paid_by": "Stanley Kubrick"
+#                 },
+#                 {
+#                     "item": "River Cruise",
+#                     "amount": "1,900.00****",
+#                     "date": "2011-10-19",
+#                     "paid_by": "Stanley Kubrick"
+#                 },
+#                 {
+#                     "item": "Video Games",
+#                     "amount": "60.00",
+#                     "date": "2012-08-30",
+#                     "paid_by": "Heath Ledger"
+#                 },
+#                 {
+#                     "item": "Brews",
+#                     "amount": "32.00",
+#                     "date": "2012-05-01",
+#                     "paid_by": "Randy Marsh"
+#                 },
+#                 {
+#                     "item": "Golf",
+#                     "amount": "800.00",
+#                     "date": "2013-07-26",
+#                     "paid_by": "Heath Ledger"
+#                 }
+#             ]
+#         },
+#         {
+#             "date": "2023-07-25"
+#         }
+#     ]
+# }
 
 ledgerStr = json.dumps(ledgerJSON)
 
-print(f"Sending Ledger to server\n\t{ledgerStr} \n")
-clientSocket.send(ledgerStr.encode())
-print("Sent Ledger to server successfully. \n")
+ledgLen = str(len(ledgerStr))
+print(f"Sending Ledger length to server: \n\t{ledgLen} \n")
+clientSocket.send(ledgLen.encode())
 
-# RECEIVING HTML from SERVER
-html = ''
-while True:
-    res = clientSocket.recv(2024).decode()
-    html += res
+# LENGTH VERIFICATION
+lenVer = clientSocket.recv(1024).decode()
 
-    if not res:
-        print(f'HTML received from server: {html} \n')
-        # create email.html
-        filename = 'email.html'
-        file = open(filename, 'w')
-        file.write(html)
-        file.close()
-        print('email.html has been created')
-        break
+if lenVer == ledgLen:
+    print(f"Sending Ledger to server\n\t{ledgerStr} \n")
+    clientSocket.send(ledgerStr.encode())
+    print("Sent Ledger to server successfully. \n")
+
+    # RECEIVING HTML from SERVER
+    html = ''
+    while True:
+        res = clientSocket.recv(2024).decode()
+        html += res
+
+        if not res:
+            print(f'HTML received from server: {html} \n')
+            # create email.html
+            filename = 'email.html'
+            file = open(filename, 'w')
+            file.write(html)
+            file.close()
+            print('email.html has been created')
+            break
 
 
 
--- server.py
+++ server.py
@@ -2,19 +2,14 @@
 import socket
 import json
 
-# setup
-HOST = "localhost"
-PORT = 65432
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-
 def extractData(ledger):
-    title = ledger['ledger'][0]['title']
-    date = ledger['ledger'][4]['date']
-    people = [person['name'] for person in ledger['ledger'][2]['persons']]
+    ledger = ledger['ledger']
+    title = ledger['title']
+    date = ledger['date']
+    people = [person['name'] for person in ledger['people']]
     people = ', '.join(people)
-    summary = ledger['ledger'][1]['summary']
-    items = ledger['ledger'][3]['items']
+    summary = ledger['summary']
+    items = ledger['transactions']
     htmlTable = []
     for item in items:
         htmlTable.append(f"<tr><td>{item['item']}</td><td>{item['amount']}</td><td>{item['date']}</td><td>{item['paid_by']}</td></tr>")
@@ -50,21 +45,40 @@ def generateHTML(title, date, people, summary, table):
 
     return html
 
+# setup
+HOST = "localhost"
+PORT = 65432
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
 while True:
     server.listen(1)
     print(f'Server listening on port: {PORT} \n')
     commSocket, addr = server.accept()
     print(f'Connected by {addr} \n')
 
-    ledgerData = commSocket.recv(1024).decode()
-    print(f'Server received: {ledgerData}\n')
+    dataLen = int(commSocket.recv(1024).decode())
+    print(f'length of data to receive: {dataLen} \n')
+    # LENGTH VERIFICATION
+    commSocket.send(str(dataLen).encode())
+    
+    ledgerData = ''
+    while True:
+        data = commSocket.recv(1024).decode()
+        ledgerData += data
+        # print('in second while loop')
+        if len(ledgerData) == dataLen:
+            print(f'Server received: {ledgerData}\n')
+            ledgerData = json.loads(ledgerData)
 
-    ledgerData = json.loads(ledgerData)
+            title, date, people, summary, htmlTable = extractData(ledgerData)
+            html = generateHTML(title, date, people, summary, htmlTable)
 
-    title, date, people, summary, htmlTable = extractData(ledgerData)
-    html = generateHTML(title, date, people, summary, htmlTable)
+            commSocket.send(html.encode())
+            print('sending html')
+            commSocket.close()
+            print('Connection closed.')
+            break
 
-    commSocket.send(html.encode())
-    print('sending html')
-    commSocket.close()
-    print('Connection closed.')
+        
+    
